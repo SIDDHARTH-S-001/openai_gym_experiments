cb090907403b58883c1f27a3faa3b33decb0038a
SIDDHARTH-S-001 <sid2k01@gmail.com>
2024-07-06 20:42:22 +0530

    modified rlocus matlab script

--- matlab/control_systems/root_locus_plot.m
+++ matlab/control_systems/root_locus_plot.m
@@ -7,4 +7,21 @@ rlocus(sys) % This function plots the root locus of the system
 [r, k] = rlocus(sys) % This function extract the closed-loop poles and associated feedback gain values 
 
 %% Closed-Loop Pole Locations for a Set of Feedback Gain Values
+num1 = [0.5 0 -1];
+den1 = [4 0 3  0 2];
+sys1 = tf(num1, den1);
+
+% considering a range of values for feedback gain K
+k = (1:0.5:5); % a values between 1 and 5 with increments of 0.5
+r = rlocus(sys1, k);
+size(r) % there are 4 closed loop poles and 9 specific gain values (column)
+% even trajectory of r-locus for specific gain values can be visualized
+rlocus(sys1, k)
+%% SISO Tool based visualization
+num2 = [1 2];
+den2 = [1 4 5 6 3];
+sys2 = tf(num2, den2);
+
+
+
 
