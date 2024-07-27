8d707f5cfd4712a74c701cbe48206eb2e52e8052
SIDDHARTH-S-001 <sid2k01@gmail.com>
2024-07-27 20:00:56 +0530

    updated transient response code, obtained response to ramp function rep as an arbitrary function

--- matlab/control_systems/transient_response_analysis.m
+++ matlab/control_systems/transient_response_analysis.m
@@ -190,8 +190,21 @@ title('Unit-Ramp Response')
 xlabel('t Sec')
 ylabel('Input and Output')
 
+%% Q9 - Response to an arbitrary input
 % function lsim can be used to get response of any arbitrary input.
 
+num = [2 1];
+den = [1 1 1];
+t = 0:0.1:10;
+r = t; % defining input as a function of time (here its same as ramp function)
+y = lsim(num,den,r,t);
+plot(t,r,'-',t,y,'o')
+grid
+title('Unit-Ramp Response Obtained by Use of Command "lsim"')
+xlabel('t Sec')
+ylabel('Unit-Ramp Input and System Output')
+text(6.3,4.6,'Unit-Ramp Input')
+text(4.75,9.0,'Output')
 
 
 
