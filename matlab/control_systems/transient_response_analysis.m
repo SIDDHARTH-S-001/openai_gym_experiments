eb5b51f6944029ab7a3fc581a4af1297c1f83cc1
SIDDHARTH-S-001 <sid2k01@gmail.com>
2024-07-27 19:58:33 +0530

    updated transient response code, obtained ramp response of state space model

--- matlab/control_systems/transient_response_analysis.m
+++ matlab/control_systems/transient_response_analysis.m
@@ -164,7 +164,33 @@ title('Unit ramp response of G(s)')
 xlabel('t Sec')
 ylabel('Input and Output')
 
+%% Q8 - Ramp response of a state space model
+% The unit-ramp response is obtained by adding a new state variable x3. 
+% The dimension of the state equation is enlarged by one.
 
+A = [ 0  1;
+     -1 -1];
+B = [0;
+     1];
+C = [1 0];
+D = 0;
+
+% Enter matrices AA, BB, CC, and DD of the new, enlarged state equation and output equation
+AA = [A zeros(2,1);C 0];
+BB = [B;0];
+CC = [0 0 1];
+DD = 0;
+
+step(AA, BB, CC, DD);
+% [z, x, t] = step(AA, BB, CC, DD);
+x3 = [0 0 1]*x'; plot(t,x3,'o',t,t,'-')
+
+grid
+title('Unit-Ramp Response')
+xlabel('t Sec')
+ylabel('Input and Output')
+
+% function lsim can be used to get response of any arbitrary input.
 
 
 
