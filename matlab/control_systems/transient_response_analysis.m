264f4c59b4ff8e82954ff67f807cd63d20dbea08
SIDDHARTH-S-001 <sid2k01@gmail.com>
2024-07-27 19:25:35 +0530

    updated transient response code, obtained response params

--- matlab/control_systems/transient_response_analysis.m
+++ matlab/control_systems/transient_response_analysis.m
@@ -96,9 +96,31 @@ xlabel('t - sec')
 ylabel('\zeta')
 zlabel('response')
 
+%% Q5 - Getting system response parameters
+num = 25;
+den = [1 6 25];
+t = 0:0.005:5
 
+[y,x,t] = step(num, den, t);
+r = 1; 
 
+while y(r) < 1.0001
+    r = r+1;
+end
 
+rise_time = (r-1)*0.005
+
+[ymax, tp] = max(y);
+peak_time = (tp-1)*0.005
+
+max_overshoot = ymax - 1
+
+s = 1001;
+while y(s) > 0.98 && y(s) < 1.02
+    s = s-1;
+end
+
+settling_time = (s-1)*0.005
 
 
 
