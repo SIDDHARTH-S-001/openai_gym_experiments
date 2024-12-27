7589dfd40776f960954cdbed2ea21f4ed88b9913
SIDDHARTH-S-001 <sid2k01@gmail.com>
2024-12-27 18:58:02 +0000

    coded det and mat inv method for finding solutions of linear equations

--- matlab/math/linear_algebra.m
+++ matlab/math/linear_algebra.m
@@ -1,5 +1,6 @@
 %% This script involes exercises from BS Greval Higher Engineering Mathematics Book - Linear Algebra
 %% Example 2.29 - solution of linear equations
+% The techniques below work when the linear equations don't form a singular matrix and the number of known variabes equal to no of equations
 % Determinant-cramers rule 
 A = [3 1 2;
      2 -3 -1;
